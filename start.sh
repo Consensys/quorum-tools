a2552180fb51d3ebf7730e8ebdd6f81add17d82d
Brian Schroeder <bts@gmail.com>
2016-11-07 15:58:39 -0500

    Use static-nodes.json and separate setup/start

--- start.sh
+++ start.sh
@@ -14,14 +14,23 @@ function cleanup {
     kill $geth3pid &
 }
 
-trap cleanup EXIT
-rm -rf gdata
+function get_enode {
+    node_number=$1
+    eval geth="\$geth${node_number}"
+    ${geth} js <(echo "console.log(admin.nodeInfo.enode)")
+}
 
-args="--networkid 1418 --nodiscover --maxpeers 10 --rpc --rpccorsdomain '*' --rpcaddr localhost --fakepow --premine --blocktime 0 --blockjitter 0"
+trap cleanup EXIT
+
+args="--networkid 1418 --nodiscover --maxpeers 10 --rpc --rpccorsdomain '*' --rpcaddr localhost"
 
 geth1="geth --datadir gdata/geth1 --port 30401 --rpcport 40401 $args $@"
 geth2="geth --datadir gdata/geth2 --port 30402 --rpcport 40402 $args $@"
 geth3="geth --datadir gdata/geth3 --port 30403 --rpcport 40403 $args $@"
+
+# SET UP NODES
+
+rm -rf gdata
 
 $geth1 init genesis.json
 $geth2 init genesis.json
@@ -31,27 +40,27 @@ $geth1 --password <(echo abcd) account new
 $geth2 --password <(echo abcd) account new
 $geth3 --password <(echo abcd) account new
 
+geth1Id=$(get_enode 1)
+geth2Id=$(get_enode 2)
+geth3Id=$(get_enode 3)
+
+echo "[\"${geth2Id}\", \"${geth3Id}\"]" > gdata/geth1/static-nodes.json
+echo "[\"${geth1Id}\", \"${geth3Id}\"]" > gdata/geth2/static-nodes.json
+echo "[\"${geth1Id}\", \"${geth2Id}\"]" > gdata/geth3/static-nodes.json
+
+# START NODES
+
 $geth1 >geth1.out 2>&1 &
+# $geth1 >/dev/null 2>&1 &
 geth1pid=$!
 
 $geth2 >geth2.out 2>&1 &
+# $geth2 >/dev/null 2>&1 &
 geth2pid=$!
 
 $geth3 >geth3.out 2>&1 &
+# $geth3 >/dev/null 2>&1 &
 geth3pid=$!
-
-# Connect the peers
-sleep 2
-geth1Id=$(geth --exec "admin.nodeInfo.enode;" attach ipc:gdata/geth1/geth.ipc)
-geth2Id=$(geth --exec "admin.nodeInfo.enode;" attach ipc:gdata/geth2/geth.ipc)
-geth3Id=$(geth --exec "admin.nodeInfo.enode;" attach ipc:gdata/geth3/geth.ipc)
-
-echo "Adding peer to geth2: $geth1Id"
-geth --exec "admin.addPeer($geth1Id);" attach ipc:gdata/geth2/geth.ipc
-echo "Adding peer to geth3: $geth1Id"
-geth --exec "admin.addPeer($geth1Id);" attach ipc:gdata/geth3/geth.ipc
-echo "Adding peer to geth3: $geth2Id"
-geth --exec "admin.addPeer($geth2Id);" attach ipc:gdata/geth3/geth.ipc
 
 sleep 1
 
