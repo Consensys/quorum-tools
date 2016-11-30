3d4d02d9d09c424c5b913492e0c94e1d239e73b9
Joel Burget <joelburget@gmail.com>
2016-11-30 13:53:34 -0500

    Don't ask for sudo password.
    `-n` is for "non-interactive", ie don't ask for a password, exit if one
    is required.

--- src/PacketFilter.hs
+++ src/PacketFilter.hs
@@ -72,7 +72,7 @@ acquirePfHelper =
       findToken :: Fold (Either Text Text) PfToken
       findToken = Fold step mempty forceFirst
 
-  in fold (inshellWithErr "sudo pfctl -E" "") findToken
+  in fold (inshellWithErr "sudo -n pfctl -E" "") findToken
 
 -- | Turn on pf and initialize with empty rules for every geth node.
 --
@@ -82,14 +82,14 @@ acquirePf geths = do
   -- enable pf / increment its enable reference count, then release on exit
   _ <- using $ managed $ bracket
     acquirePfHelper
-    (\(PfToken tk) -> sh $ inshellWithNoErr (format ("sudo pfctl -X "%s) tk) "")
+    (\(PfToken tk) -> sh $ inshellWithNoErr (format ("sudo -n pfctl -X "%s) tk) "")
 
   -- write the initial ruleset which passes packets through to each geth
   ruleFile <- using $ fileContaining $ pure (emptyRuleset geths)
-  view $ inshellWithNoErr (format ("sudo pfctl -f "%fp) ruleFile) ""
+  view $ inshellWithNoErr (format ("sudo -n pfctl -f "%fp) ruleFile) ""
 
   -- flush the rules we added on exit
-  _ <- using $ managed (onExit (sh $ inshellWithNoErr "sudo pfctl -a 'raft-anchor' -F rules" ""))
+  _ <- using $ managed (onExit (sh $ inshellWithNoErr "sudo -n pfctl -a 'raft-anchor' -F rules" ""))
   return ()
 
 matchOnce :: Pattern a -> Text -> Maybe a
@@ -165,10 +165,10 @@ partition (Millis ms) geth = do
   ports <- getPortsForGeth gdata geth
   let anchor = format ("raft-anchor/geth"%d) (gId geth)
   _ <- sh $ inshellWithNoErr
-    (format ("sudo pfctl -a "%s%" -f -") anchor)
+    (format ("sudo -n pfctl -a "%s%" -f -") anchor)
     (select [blockPortsRule ports])
 
   -- make sure to reset pf.conf on exit
-  _ <- using $ managed (onExit (sh $ inshellWithNoErr "sudo pfctl -a raft-partition" ""))
+  _ <- using $ managed (onExit (sh $ inshellWithNoErr "sudo -n pfctl -a raft-partition" ""))
 
   liftIO $ threadDelay (1000 * ms)
