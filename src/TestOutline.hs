dae7fa0881dd5433811917ff1e82f173c4d41554
Joel Burget <joelburget@gmail.com>
2016-12-14 13:41:19 -0500

    Tear down the raft node every test repeat.

--- src/TestOutline.hs
+++ src/TestOutline.hs
@@ -3,8 +3,8 @@
 module TestOutline where
 
 import Control.Concurrent         (threadDelay)
-import Control.Concurrent.Async   (Async, cancel)
-import Control.Concurrent.MVar    (MVar, readMVar)
+import Control.Concurrent.Async   (cancel)
+import Control.Concurrent.MVar    (readMVar)
 import Control.Monad.Managed      (MonadManaged)
 import Control.Monad.Reader       (ReaderT (runReaderT), MonadReader)
 import System.Info
@@ -24,55 +24,36 @@ repeatTester
   -> NumNodes
   -> ([Geth] -> ReaderT ClusterEnv Shell ())
   -> IO ()
-repeatTester repeatNum numNodes cb = sh $ do
-  (lastBlocks, terminatedAsyncs, nodes) <-
-    runReaderT (setupNodesForTester numNodes) defaultClusterEnv
-  repeatTesterHelper repeatNum nodes terminatedAsyncs lastBlocks cb
+repeatTester (Repeat 0) _ _ = return ()
+repeatTester (Repeat n) numNodes cb = do
+  sh $ flip runReaderT defaultClusterEnv $ do
+    let geths = [1..GethId (unNumNodes numNodes)]
+    _ <- when (os == "darwin") PF.acquirePf
 
--- | Run this test once
+    nodes <- setupNodes geths
+    (readyAsyncs, terminatedAsyncs, lastBlocks) <-
+      unzip3 <$> traverse runNode nodes
+
+    -- wait for geth to launch, then unlock and start raft
+    awaitAll readyAsyncs
+
+    cb nodes
+
+    void $ liftIO $ do
+      -- HACK: wait three seconds for geths to catch up
+      threadDelay (3 * second)
+
+      -- verify that all have consistent logs
+      lastBlocks' <- traverse readMVar lastBlocks
+      print $ verifySameLastBlock lastBlocks'
+
+      -- cancel all the workers
+      mapM_ cancel terminatedAsyncs
+
+  repeatTester (Repeat (n - 1)) numNodes cb
+
 tester :: NumNodes -> ([Geth] -> ReaderT ClusterEnv Shell ()) -> IO ()
 tester = repeatTester (Repeat 1)
-
-setupNodesForTester
-  :: (MonadManaged m, HasEnv m)
-  => NumNodes
-  -> m ([MVar LastBlock], [Async NodeTerminated], [Geth])
-setupNodesForTester (NumNodes n) = do
-  let geths = [1..GethId n]
-  _ <- when (os == "darwin") PF.acquirePf
-
-  nodes <- setupNodes geths
-  (readyAsyncs, terminatedAsyncs, lastBlocks) <-
-    unzip3 <$> traverse runNode nodes
-
-  -- wait for geth to launch, then unlock and start raft
-  awaitAll readyAsyncs
-  startRaftAcross nodes
-  return (lastBlocks, terminatedAsyncs, nodes)
-
-repeatTesterHelper
-  :: Repeat
-  -> [Geth]
-  -> [Async NodeTerminated]
-  -> [MVar LastBlock]
-  -> ([Geth] -> ReaderT ClusterEnv Shell ())
-  -> Shell ()
-repeatTesterHelper (Repeat 0) _ _ _ _ = return ()
-repeatTesterHelper (Repeat n) geths terminatedAsyncs lastBlocks cb = do
-  runReaderT (cb geths) defaultClusterEnv
-
-  -- HACK: wait three seconds for geths to catch up
-  liftIO $ threadDelay (3 * second)
-
-  -- verify that all have consistent logs
-  lastBlocks' <- traverse (liftIO . readMVar) lastBlocks
-  let verified = verifySameLastBlock lastBlocks'
-  liftIO $ print verified
-
-  case verified of
-    Verified ->
-      repeatTesterHelper (Repeat (n - 1)) geths terminatedAsyncs lastBlocks cb
-    _ -> mapM_ (liftIO . cancel) terminatedAsyncs -- cancel all the workers
 
 partition :: (MonadManaged m, HasEnv m) => Millis -> GethId -> m ()
 partition millis node =
