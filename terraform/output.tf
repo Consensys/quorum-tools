585c048a737a00ad25fd829afcbc95e21afee22c
Brian Schroeder <bts@gmail.com>
2017-02-16 13:04:49 -0500

    Add multi-region-friendly outputs

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,8 +1,11 @@
+output "multi-region-cluster" {
+  value = "${var.multi_region}"
+}
+output "aws-region" {
+  value = "${var.aws_region}"
+}
 output "environment" {
   value = "${var.env}"
-}
-output "quorum-public-ips" {
-  value = ["${aws_instance.quorum.*.public_ip}"]
 }
 output "quorum-private-ips" {
   value = ["${aws_instance.quorum.*.private_ip}"]
@@ -11,6 +14,13 @@ output "quorum-azs" {
   value = ["${aws_instance.quorum.*.availability_zone}"]
 }
 
-output "geth1" { value = "${aws_instance.quorum.0.public_ip}" }
-output "geth2" { value = "${aws_instance.quorum.1.public_ip}" }
-output "geth3" { value = "${aws_instance.quorum.2.public_ip}" }
+# Yes, this is insane. It's not easy with this language. This yields an EIP if it was allocated, otherwise standard public IP if it was allocated, or an empty string. For some reason accessing the EIP list out of bounds always gives you the last element if one exists. Also I can't figure out how to prevent some (e.g. "empty") outputs from showing up.
+output "geth1" {
+  value = "${length(var.quorum_eip_ids) > (0 + var.first_geth_id - 1) ? element(aws_eip_association.quorum_eip_association.*.public_ip, (0 + var.first_geth_id - 1)) : (var.num_instances > 0 ? element(aws_instance.quorum.*.public_ip, (0 + var.first_geth_id - 1)) : "")}"
+}
+output "geth2" {
+  value = "${length(var.quorum_eip_ids) > (1 + var.first_geth_id - 1) ? element(aws_eip_association.quorum_eip_association.*.public_ip, (1 + var.first_geth_id - 1)) : (var.num_instances > 1 ? element(aws_instance.quorum.*.public_ip, (1 + var.first_geth_id - 1)) : "")}"
+}
+output "geth3" {
+  value = "${length(var.quorum_eip_ids) > (2 + var.first_geth_id - 1) ? element(aws_eip_association.quorum_eip_association.*.public_ip, (2 + var.first_geth_id - 1)) : (var.num_instances > 2 ? element(aws_instance.quorum.*.public_ip, (2 + var.first_geth_id - 1)) : "")}"
+}
