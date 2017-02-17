db8b5d60990cfab2e1e683351b59dc7c3011c4ce
Brian Schroeder <bts@gmail.com>
2017-02-17 13:37:51 -0500

    Add multi-region support to aws-spam

--- src/Mains/AwsSpam.hs
+++ src/Mains/AwsSpam.hs
@@ -4,42 +4,45 @@ module Mains.AwsSpam where
 
 import           Control.Monad.Reader (runReaderT)
 import           Control.RateLimit    (RateLimit)
+import           Data.Bool            (bool)
 import           Data.Time.Units      (Millisecond)
 import           Turtle
 
 import           Cluster
-import           Cluster.Types
 import           Cluster.Aws          (internalAwsIp)
 import           Cluster.Client       (loadLocalNode, perSecond, spamGeth)
+import           Cluster.Types
 
---
--- NOTE: this could wait until the local node says that the cluster has a leader
---
---       if we wanted to do more realistic workload simulation, we could sample
---         tx sends from a poisson distribution.
---
+data SpamConfig = SpamConfig { rateLimit   :: RateLimit Millisecond
+                             , clusterType :: AwsClusterType
+                             }
 
-cliParser :: Parser (RateLimit Millisecond)
-cliParser = perSecond <$>
-  optInteger "rps"  'r' "The number of requests per second"
+cliParser :: Parser SpamConfig
+cliParser = SpamConfig
+  <$> fmap perSecond (optInteger "rps"  'r' "The number of requests per second")
+  <*> fmap (bool SingleRegion MultiRegion)
+           (switch  "multi-region" 'g' "Whether the cluster is multi-region")
 
-cEnv :: GethId -> ClusterEnv
-cEnv gid = mkClusterEnv (internalAwsIp maxClusterSize numSubnets)
-                        mkDataDir
-                        [gid]
+cEnv :: AwsClusterType -> GethId -> ClusterEnv
+cEnv cType gid = mkClusterEnv mkIp mkDataDir [gid]
   where
-    -- We don't need the exact cluster size here; just something higher than the
-    -- geth ID we want to spam:
+    --
+    -- TODO: fix/improve this
+    --
     maxClusterSize = 10
     numSubnets     = 3
     mkDataDir = const $ DataDir "/datadir"
+
+    mkIp = case cType of
+      SingleRegion -> internalAwsIp maxClusterSize numSubnets
+      MultiRegion  -> const $ Ip "127.0.0.1"
 
 readGidFromHomedir :: IO GethId
 readGidFromHomedir = GethId . read <$> readFile "/home/ubuntu/node-id"
 
 awsSpamMain :: IO ()
 awsSpamMain = do
-  rateLimit <- options "Spams the local node with public transactions" cliParser
+  config <- options "Spams the local node with public transactions" cliParser
   gid <- readGidFromHomedir
-  geth <- runReaderT (loadLocalNode gid) (cEnv gid)
-  spamGeth geth rateLimit
+  geth <- runReaderT (loadLocalNode gid) (cEnv (clusterType config) gid)
+  spamGeth geth (rateLimit config)
