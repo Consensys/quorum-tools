2f61dc9c728bff194e7639e5366ece82ae1a7b4d
Brian Schroeder <bts@gmail.com>
2017-02-23 15:54:12 -0500

    Reduce/improve output of provisioning scripts

--- terraform/scripts/fetch-images.sh
+++ terraform/scripts/fetch-images.sh
@@ -3,7 +3,11 @@
 set -e
 set -u
 
-eval `aws ecr get-login --region us-east-1 | sed 's/^docker/sudo docker/'`
-sudo docker pull 128367567151.dkr.ecr.us-east-1.amazonaws.com/quorum-raft:latest
-sudo docker pull 128367567151.dkr.ecr.us-east-1.amazonaws.com/quorum-constellation:latest
-sudo docker pull 128367567151.dkr.ecr.us-east-1.amazonaws.com/quorum-harness:latest
+echo "fetching docker images"
+
+eval `aws ecr get-login --region us-east-1 | sed 's/^docker/sudo docker/'`                >/dev/null
+sudo docker pull 128367567151.dkr.ecr.us-east-1.amazonaws.com/quorum-raft:latest          >/dev/null
+sudo docker pull 128367567151.dkr.ecr.us-east-1.amazonaws.com/quorum-constellation:latest >/dev/null
+sudo docker pull 128367567151.dkr.ecr.us-east-1.amazonaws.com/quorum-harness:latest       >/dev/null
+
+echo "fetching complete"
--- terraform/scripts/start-quorum.sh
+++ terraform/scripts/start-quorum.sh
@@ -6,4 +6,6 @@ set -u
 # NOTE: this only works for clusters of size 9 or smaller:
 ID=$(cat node-id)
 
-sudo docker run -d -p 3040$ID:3040$ID -p 4040$ID:4040$ID -p 5040$ID:5040$ID -v /home/ubuntu/datadir:/datadir -v /home/ubuntu/password:/password 128367567151.dkr.ecr.us-east-1.amazonaws.com/quorum-raft /bin/sh -c "geth --datadir /datadir --port 3040$ID --rpcport 4040$ID --networkid 1418 --verbosity 3 --nodiscover --rpc --rpccorsdomain '*' --rpcaddr '0.0.0.0' --raft --unlock 0 --password /password"
+echo "starting geth ${ID}"
+
+sudo docker run -d -p 3040$ID:3040$ID -p 4040$ID:4040$ID -p 5040$ID:5040$ID -v /home/ubuntu/datadir:/datadir -v /home/ubuntu/password:/password -e PRIVATE_CONFIG='/datadir/constellation.toml' 128367567151.dkr.ecr.us-east-1.amazonaws.com/quorum-raft /bin/sh -c "geth --datadir /datadir --port 3040$ID --rpcport 4040$ID --networkid 1418 --verbosity 3 --nodiscover --rpc --rpccorsdomain '*' --rpcaddr '0.0.0.0' --raft --unlock 0 --password /password"
