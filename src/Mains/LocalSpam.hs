bfa2b30b45a54026756e15365a6f63c7def85484
Joel Burget <joelburget@gmail.com>
2017-02-23 14:43:22 -0500

    Add address options to local/aws -spam.

--- src/Mains/LocalSpam.hs
+++ src/Mains/LocalSpam.hs
@@ -4,54 +4,26 @@ module Mains.LocalSpam where
 
 import           Control.Monad.Reader (runReaderT)
 import           Control.RateLimit    (RateLimit)
-import qualified Data.Text            as T
 import           Data.Time.Units      (Millisecond)
 import           Turtle
 
 import           Cluster              (mkLocalEnv)
 import           Cluster.Types
-import           Cluster.Client       (BenchType(..), loadLocalNode, spamGeth,
+import           Cluster.Client       (loadLocalNode, spamGeth,
                                        perSecond)
-import           Cluster.Util         (Bytes20, bytes20P)
-import           SharedPartitioning   (matchOnce)
-
-hexLike :: Pattern a -> Pattern a
-hexLike = (optional "0x" >>)
-
--- TODO: restrict more than chars1 -- figure out what characters are allowed in
--- a signature
-contractPattern :: Pattern (Bytes20, UnencodedMethod)
-contractPattern = hexLike $ (,) <$> bytes20P <*> fmap UnencodedMethod chars1
-
-addrP :: Pattern Addr
-addrP = hexLike $ Addr . T.pack <$> count 40 hexDigit
-
-privateForPattern :: Pattern [Addr]
-privateForPattern = addrP `sepBy` ","
-
-processContractArgs :: Maybe Text -> Maybe Text -> BenchType
-processContractArgs contractT privateForT = maybe BenchEmptyTx BenchTx $ do
-  rawContract <- contractT
-  rawPrivateFor <- privateForT
-
-  let privacy = case matchOnce privateForPattern rawPrivateFor of
-        Nothing -> Public
-        Just addrs -> PrivateFor addrs
-
-  (addr, method) <- matchOnce contractPattern rawContract
-  pure $ Tx (Just addr) method privacy SendTransactionAsync
+import           Cluster.SpamArgs
 
 cliParser :: Parser (GethId, RateLimit Millisecond, Maybe Text, Maybe Text)
-cliParser = (,,,) <$> gethIdP <*> rateLimitP <*> optional contractP <*> optional privateForP
+cliParser = (,,,)
+  <$> gethIdP
+  <*> rateLimitP
+  <*> optional contractP
+  <*> optional privateForP
   where
     gethIdP = GethId <$>
       optInt     "geth" 'g' "The Geth ID of the local node to spam"
     rateLimitP = perSecond <$>
       optInteger "rps"  'r' "The number of requests per second"
-    contractP = optText "contract" 'c'
-      "Contract address and method <addr>:increment()"
-    privateForP = optText "privatefor" 'p'
-      "Comma-separated addresses with access to this transaction"
 
 localSpamMain :: IO ()
 localSpamMain = do
