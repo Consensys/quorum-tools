fc662217a3272398742e912afd855dad64634268
Joel Burget <joelburget@gmail.com>
2017-03-03 10:50:18 -0500

    Simplify inbox contract.

--- messaging-demo/inbox.sol
+++ messaging-demo/inbox.sol
@@ -1,23 +1,12 @@
 pragma solidity ^0.4.9;
 
 contract Inbox {
-    string lastMessage;
-
     event Message(
         address indexed from,
         string message
     );
 
-    function Inbox() {
-        lastMessage = "inbox empty!";
-    }
-
     function sendMessage(string message) {
-        lastMessage = message;
         Message(msg.sender, message);
-    }
-
-    function readMessage() returns (string) {
-        return lastMessage;
     }
 }
