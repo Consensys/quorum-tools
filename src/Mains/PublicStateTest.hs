aa1426d3fe62a8f489e494f817bae74bb68cf191
Brian Schroeder <bts@gmail.com>
2017-03-29 17:10:00 -0400

    Wait before verifying public state converged

--- src/Mains/PublicStateTest.hs
+++ src/Mains/PublicStateTest.hs
@@ -13,12 +13,20 @@ import           TestOutline              hiding (verify)
 publicStateTestMain :: IO ()
 publicStateTestMain = testNTimes 5 PrivacyDisabled (NumNodes 3) $ \iNodes -> do
   let (geth1, geth1Instruments) = head iNodes
-      sendTo = cycle (fst <$> iNodes)
+      geths = fst <$> iNodes
+      sendTo = cycle geths
       contract = simpleStorage Public
 
   storageAddr <- createContract geth1 contract (txAddrs geth1Instruments)
 
-  forM_ (zip [1..10] sendTo) $ \(no, geth) -> do
+  let increments = 10
+
+  forM_ (take increments sendTo) $ \geth ->
     incrementStorage geth contract storageAddr
+  td 1
+
+  let expectedValue = 42 + increments
+
+  forM_ geths $ \geth -> do
     i <- getStorage geth contract storageAddr
-    expectEq i (42 + no)
+    expectEq i expectedValue
