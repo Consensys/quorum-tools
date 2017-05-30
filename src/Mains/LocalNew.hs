b64427c0d3d86df9891f36fa957702bf375a28bb
Brian Schroeder <bts@gmail.com>
2017-05-30 12:14:25 -0400

    Add cluster size options to local-new

--- src/Mains/LocalNew.hs
+++ src/Mains/LocalNew.hs
@@ -5,28 +5,65 @@ module Mains.LocalNew where
 
 import           Control.Lens         (view, (.~))
 import           Control.Monad.Reader (runReaderT)
+import           Data.Maybe           (fromMaybe)
+import           Data.Optional        (Optional(Specific))
+import qualified Data.Set             as Set
+import qualified Data.Text            as T
 import           Turtle               hiding (view)
+import           Turtle.Options       (HelpMessage(..))
 
-import           Cluster              (generateClusterKeys, mkLocalEnv,
-                                       runNodesIndefinitely, wipeAndSetupNodes)
+import           Cluster              (generateClusterKeys, mkLocalEnv, runNode,
+                                       wipeAndSetupNodes)
+import           Cluster.Control      (awaitAll)
 import           Cluster.Types
 import           Constellation
 
+data LocalNewConfig
+  = LocalNewConfig { totalPeers   :: Int
+                   , initialPeers :: Maybe Int
+                   }
+
+defaultClusterSize :: Int
+defaultClusterSize = 3
+
+cliParser :: Parser LocalNewConfig
+cliParser = LocalNewConfig
+    <$> (optInt "nodes" 'n' nodesMessage <|> pure defaultClusterSize)
+    <*> optional (optInt "initial" 'i' initialPeersMessage)
+
+  where
+    nodesMessage = Specific . HelpMessage $
+      "The total number of peers. Default: " <> T.pack (show defaultClusterSize)
+    initialPeersMessage =
+      "The number of initial peers. Default: the total number of peers."
+
 localNewMain :: IO ()
 localNewMain = do
-    keys <- generateClusterKeys clusterSize password
-    let cEnv = mkLocalEnv keys
+    config <- parseConfig
+
+    let totalSize = totalPeers config
+        initialSize = fromMaybe totalSize (initialPeers config)
+
+    when (totalSize < initialSize) $
+      error "initial peers can not be greater than total peers"
+
+    keys <- generateClusterKeys totalSize password
+    let gids = clusterGids totalSize
+        cEnv = mkLocalEnv keys
              & clusterPrivacySupport .~ PrivacyEnabled
+             & clusterInitialMembers .~ Set.fromList (take initialSize gids)
              & clusterPassword       .~ password
 
     sh $ flip runReaderT cEnv $ do
-      geths <- wipeAndSetupNodes Nothing "gdata" (clusterGids clusterSize)
+      geths <- wipeAndSetupNodes Nothing "gdata" gids
 
       privacySupport <- view clusterPrivacySupport
       when (privacySupport == PrivacyEnabled) (startConstellationNodes geths)
 
-      runNodesIndefinitely geths
+      instruments <- traverse (runNode totalSize) geths
+
+      awaitAll $ nodeTerminated <$> instruments
 
   where
     password    = CleartextPassword "abcd"
-    clusterSize = 3
+    parseConfig = options "Creates a new local cluster" cliParser
