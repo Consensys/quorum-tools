86c4a32ec3e48aa88f68fac26a6035298c1b9479
Brian Schroeder <bts@gmail.com>
2017-08-11 14:06:41 -0400

    Add new Behavior combinators

--- src/QuorumTools/Control.hs
+++ src/QuorumTools/Control.hs
@@ -1,20 +1,25 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections       #-}
 
 module QuorumTools.Control where
 
 import           Control.Concurrent.Async     (Async)
 import           Control.Concurrent.MVar      (newEmptyMVar, takeMVar,
                                                tryPutMVar)
-import           Control.Concurrent.STM       (atomically)
+import           Control.Concurrent.STM       (STM, atomically)
 import           Control.Concurrent.STM.TChan (TChan, dupTChan, newTChan,
                                                readTChan, writeTChan)
 import           Control.Concurrent.STM.TMVar (TMVar, newTMVar, putTMVar,
-                                               readTMVar, takeTMVar)
+                                               readTMVar, swapTMVar, takeTMVar)
 import           Control.Exception            (bracket)
+import           Control.Monad                (forever, (<$!>))
 import           Control.Monad.Loops          (untilJust)
 import           Control.Monad.Managed        (MonadManaged)
-import           Data.Foldable                (traverse_)
-import           Data.Maybe                   (fromMaybe)
+import           Data.Foldable                (for_, toList, traverse_)
+import           Data.Maybe                   (fromJust, fromMaybe)
+import           Data.Vector                  (Vector)
+import qualified Data.Vector                  as V
 import           Turtle                       (Fold (..), MonadIO, fork, liftIO,
                                                void, wait)
 
@@ -52,31 +57,94 @@ event e = do
   let fire = void $ tryPutMVar mvar e
   pure (occurred, fire)
 
--- | Creates a stream of values for a many publishers to update, and many
+-- | Creates a stream of values for many publishers to update, and many
 -- consumers to subscribe (to value changes) or observe the current value.
 behavior :: (MonadIO m) => m (Behavior a)
 behavior = liftIO $ atomically $ Behavior <$> newTChan <*> newTMVar Nothing
 
 transition' :: MonadIO m => Behavior a -> (Maybe a -> a) -> m ()
 transition' (Behavior tc tm) f = liftIO $ atomically $ do
-  prev <- takeTMVar tm
-  let next = f prev
-  putTMVar tm $ Just next
-  writeTChan tc next
+  v <- takeTMVar tm
+  let v' = f v
+  putTMVar tm $ Just v'
+  writeTChan tc v'
 
 transition :: (MonadIO m, Monoid a) => Behavior a -> (a -> a) -> m ()
 transition b f = transition' b (f . fromMaybe mempty)
 
-subscribe :: MonadIO m => Behavior a -> m (TChan a)
-subscribe (Behavior chan _) = liftIO $ atomically $ dupTChan chan
+subscribe' :: Behavior a -> STM (TChan a)
+subscribe' (Behavior chan _) = dupTChan chan
 
-observe' :: MonadIO m => Behavior a -> m (Maybe a)
-observe' (Behavior _ mvar) = liftIO $ atomically $ readTMVar mvar
+subscribe :: MonadIO m => Behavior a -> m (TChan a)
+subscribe = liftIO . atomically . subscribe'
+
+observe' :: Behavior a -> STM (Maybe a)
+observe' (Behavior _ mvar) = readTMVar mvar
 
 observe :: (MonadIO m, Monoid a) => Behavior a -> m a
-observe = fmap (fromMaybe mempty) . observe'
+observe = fmap (fromMaybe mempty) . liftIO . atomically . observe'
 
 watch :: MonadManaged m => Behavior a -> (a -> Maybe b) -> m (Async b)
 watch b decide = do
   chan <- subscribe b
   fork $ liftIO $ untilJust $ decide <$> atomically (readTChan chan)
+
+nextFrom :: MonadManaged m => Behavior a -> m (Async a)
+nextFrom b = watch b Just
+
+-- forkReplication :: MonadManaged m => forall r. (a -> IO b) -> Behavior a -> Behavior b -> m (Async r)
+-- forkReplication act upstream downstream = fork $ do
+--   chan <- subscribe upstream
+--   forever $ do
+--     val <- atomically $ readTChan chan
+--     val' <- act val
+--     transition' downstream (const val')
+
+mapB :: (MonadManaged m) => (a -> b) -> Behavior a -> m (Behavior b)
+mapB f upstream = do
+    downstream <- behavior
+    void $ forkReplication downstream
+    return downstream
+
+  where
+    forkReplication downstream@(Behavior _ downstreamTm) = fork $ do
+      chan <- atomically $ do
+        mv0 <- observe' upstream
+        void $ swapTMVar downstreamTm (f <$!> mv0)
+        subscribe' upstream
+      forever $ do
+        val <- atomically $ readTChan chan
+        -- NOTE: we publish downstream asychronously here
+        let val' = f $! val
+        transition' downstream (const val')
+
+-- -- | When any of the input behaviors post a new value, the output behavior will
+-- -- post that value. In theory we could implement a simple 'merge' which works
+-- -- over two 'Behavior's then implement this function in terms of that, but our
+-- -- "FRP" implementation is not so efficient, so we don't have that luxury.
+-- merged :: (MonadManaged m, Foldable f) => f (Behavior a) -> m (Behavior a)
+-- merged upstreams = do
+--   downstream <- behavior
+--   for_ upstreams $ \upstream ->
+--     forkReplication pure upstream downstream
+--   return downstream
+
+combine :: forall a m t. (MonadManaged m, Traversable t)
+        => t (Behavior a)
+        -> m (Behavior (Vector (Maybe a)))
+combine upstreams = do
+    downstream <- behavior
+    startReplication downstream
+    return downstream
+
+  where
+    startReplication :: Behavior (Vector (Maybe a)) -> m ()
+    startReplication downstream@(Behavior _ downstreamTm) = do
+      chans <- fmap toList $ liftIO $ atomically $ do
+        mv0s <- traverse observe' upstreams
+        void $ swapTMVar downstreamTm $ Just $ V.fromList $ toList mv0s
+        traverse subscribe' upstreams
+      for_ (zip [(0 :: Int)..] chans) $ \(i, chan) -> fork $ forever $ do
+        val <- atomically $ readTChan chan
+        -- NOTE: we publish downstream asychronously here
+        transition' downstream $ (V.// [(i, Just val)]) . fromJust
