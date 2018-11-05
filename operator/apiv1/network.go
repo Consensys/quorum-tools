46d0b93a2a591910dc126780edf3ec96026f0f8c
Trung Nguyen <trung.n.k@gmail.com>
2018-11-05 17:17:08 -0500

    caching network

--- operator/apiv1/network.go
+++ operator/apiv1/network.go
@@ -31,9 +31,20 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/gorilla/mux"
+
+	"github.com/jpmorganchase/quorum-tools/docker"
+
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/jpmorganchase/quorum-tools/helper"
 )
+
+type networkCache struct {
+	builder         *docker.QuorumBuilder
+	operatorAddress string
+}
+
+var cache = make(map[string]*networkCache)
 
 // POST /v1/networks
 func (api *API) NewNetwork(w http.ResponseWriter, r *http.Request) {
@@ -46,60 +57,97 @@ func (api *API) NewNetwork(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Can't write", http.StatusInternalServerError)
 		return
 	}
-	log.Info("Write network config", "file", tmpFile.Name())
-	qctl, err := os.Executable()
+	builder, err := docker.NewQuorumBuilder(tmpFile)
 	if err != nil {
-		http.Error(w, "Can't run network creator", http.StatusInternalServerError)
+		http.Error(w, "Invalid template format", http.StatusBadRequest)
 		return
 	}
-	freePort := api.getFreePort()
-	// run qctl quorum -f <tmpFile> up --enable-operator --operator-port 0
-	qctlCommand := exec.Command(qctl, "quorum", "-f", tmpFile.Name(), "up", "--enable-operator", "--operator-port", fmt.Sprintf("%d", freePort))
-	qctlCommand.Stdout = os.Stdout
-	qctlCommand.Stderr = os.Stderr
-	if err := qctlCommand.Start(); err != nil {
-		log.Info("Command run failed", "error", err)
-		http.Error(w, "Server error", http.StatusInternalServerError)
-		return
-	}
-	address := fmt.Sprintf("%s:%d", regexp.MustCompile(":.+").ReplaceAllString(r.Host, ""), freePort)
-	waitForServer := &helper.StateChangeConfig{
-		PollInterval: 3 * time.Second,
-		Timeout:      5 * time.Minute,
-		Target:       []string{"up"},
-		Refresh: func() (*helper.StateResult, error) {
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				return &helper.StateResult{
-					State: "not up",
-				}, nil
-			}
-			defer conn.Close()
-			if err := qctlCommand.Process.Signal(syscall.Signal(0)); err !=  nil {
-				return nil, err
-			}
-			return &helper.StateResult{
-				State:  "up",
-				Result: "up",
-			}, nil
-		},
-	}
-	if _, err := waitForServer.Wait(); err != nil {
-		log.Info("Network is not up. Clean up", "error", err)
-		if err := exec.Command(qctl, "quorum", "-f", tmpFile.Name(), "down").Run(); err != nil {
-			log.Info("Can't clean up network", "error", err)
+	var address string
+	if nc, ok := api.getFromCache(builder.Name); ok {
+		address = nc.operatorAddress
+	} else {
+		log.Info("Write network config", "file", tmpFile.Name())
+		qctl, err := os.Executable()
+		if err != nil {
+			http.Error(w, "Can't run network creator", http.StatusInternalServerError)
+			return
 		}
-		http.Error(w, "Server error", http.StatusInternalServerError)
-		return
+		freePort := api.getFreePort()
+		// run qctl quorum -f <tmpFile> up --enable-operator --operator-port 0
+		qctlCommand := exec.Command(qctl, "quorum", "-f", tmpFile.Name(), "up", "--enable-operator", "--operator-port", fmt.Sprintf("%d", freePort))
+		qctlCommand.Stdout = os.Stdout
+		qctlCommand.Stderr = os.Stderr
+		if err := qctlCommand.Start(); err != nil {
+			log.Info("Command run failed", "error", err)
+			http.Error(w, "Server error", http.StatusInternalServerError)
+			return
+		}
+		dialAddress := fmt.Sprintf("%s:%d", regexp.MustCompile(":.+").ReplaceAllString(r.Host, ""), freePort)
+		address = fmt.Sprintf("http://%s", dialAddress)
+		waitForServer := &helper.StateChangeConfig{
+			PollInterval: 3 * time.Second,
+			Timeout:      5 * time.Minute,
+			Target:       []string{"up"},
+			Refresh: func() (*helper.StateResult, error) {
+				conn, err := net.Dial("tcp", dialAddress)
+				if err != nil {
+					return &helper.StateResult{
+						State: "not up",
+					}, nil
+				}
+				defer conn.Close()
+				if err := qctlCommand.Process.Signal(syscall.Signal(0)); err != nil {
+					return nil, err
+				}
+				return &helper.StateResult{
+					State:  "up",
+					Result: "up",
+				}, nil
+			},
+		}
+		if _, err := waitForServer.Wait(); err != nil {
+			log.Info("Network is not up. Clean up", "error", err)
+			if err := exec.Command(qctl, "quorum", "-f", tmpFile.Name(), "down").Run(); err != nil {
+				log.Info("Can't clean up network", "error", err)
+			}
+			http.Error(w, "Server error", http.StatusInternalServerError)
+			return
+		}
+		cache[builder.Name] = &networkCache{
+			builder:         builder,
+			operatorAddress: address,
+		}
 	}
 	if err := writeJSON(w, map[string]string{
-		"operator-address": fmt.Sprintf("http://%s", address),
+		"operator-address": address,
 	}); err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 }
 
+// DELETE /v1/networks/{name}
 func (api *API) DeleteNetwork(w http.ResponseWriter, r *http.Request) {
+	pathVars := mux.Vars(r)
+	name, ok := pathVars["name"]
+	if !ok {
+		http.Error(w, "Missing network name", http.StatusBadRequest)
+		return
+	}
+	c, ok := cache[name]
+	if !ok {
+		http.Error(w, "No such network", http.StatusBadRequest)
+		return
+	}
+	if err := c.builder.Destroy(true); err != nil {
+		http.Error(w, "Server error", http.StatusInternalServerError)
+		return
+	}
+}
 
+func (api *API) getFromCache(name string) (nc *networkCache, ok bool) {
+	api.Mux.Lock()
+	defer api.Mux.Unlock()
+	nc, ok = cache[name]
+	return
 }
